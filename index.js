6cc2b06b7259f7fdfe2203faa43c7a4e3d9717d1
cheese-cracker <4chinmai@gmail.com>
2018-12-24 18:34:48 +0530

    Update: Support for and added template html pages

--- index.js
+++ index.js
@@ -1,17 +1,22 @@
 const express = require('express');
 const http = require('http');
+const morgan = require('morgan');
+
 
 const hostname = 'localhost';
 const port = 3000;
 
 const app = express();
+app.use(morgan('dev'));
+
+app.use(express.static(__dirname+'/templates'));
 // next is for middleware
 app.use((req, res, next) => {
-    console.log(req.headers);
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/html');
-    res.end('<h1> Express Server Index</h1>');
+    res.end('<h1> Express Server says "ERROR"!</h1>');
 });
+
 
 // Since Express Server
 const serv = http.createServer(app);
